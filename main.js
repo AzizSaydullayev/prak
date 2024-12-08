53b1a5a77f31cbe9d669a37bb609c71421643bff
AzizSaydullayev <azizbeksaydullayv66@gmail.com>
2024-12-08 22:42:03 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+let son=prompt("Birinchi son kiriting")
+
+let son2=prompt("Ikkinchi son kiriting")
+
+let doc=prompt
+("1 qoshish 2 ayrish 3 kopaytirish 4 bolish")
+
+if(doc ==="1"){
+natija=son+son2;
+}else if (doc==="2"){
+natija=son-son2;
+}else if(doc==="3"){
+natija=son*son2;
+}else if (doc==="4"){
+natija=son/son2;
+}else{
+"Hato"
+}
+alert("Natija"+natija)
